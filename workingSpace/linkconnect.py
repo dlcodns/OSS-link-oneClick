b263a736610f5fa0b0c183c673216eed409588b0
JH_T0mmy <34836246+wogur0915@users.noreply.github.com>
2022-11-26 20:15:49 +0900

    크롬드라이버 자동설치 라이브러리 변경 및 드라이버를 실행할 때 마다 명령 프롬프트 창이 안뜨게 하는 옵션 추가

--- workingSpace/linkconnect.py
+++ workingSpace/linkconnect.py
@@ -1,35 +1,36 @@
 from tkinter import *
 from selenium.webdriver.common.keys import Keys
-#웹드라이버 자동
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-
 
 root = Tk()
 root.title("임시")
 root.geometry("300x300")
 
+chromedriver_autoinstaller.install(True)                    # 크롬 드라이버 자동 설치
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)      # 창 꺼지지 않는 옵션
+chrome_service = Service('chromedriver')
+chrome_service.creationflags = 0x08000000                   # 명령 프롬프트창 안뜨게 하는 옵션
+
+myId = '학번'
+myPw = '비밀번호'
 
 def ecampusLink() :
-    myid = '학번'
-    mypw = '비밀번호'
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", True)
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
-
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
     browser.get('https://ecampus.chungbuk.ac.kr/')
 
     id = browser.find_element(By.XPATH,'//*[@id="uid"]')
     id.click()
-    id.send_keys(myid)
+    id.send_keys(myId)
 
     pw = browser.find_element(By.XPATH,'//*[@id="pswd"]')
     pw.click()
-    pw.send_keys(mypw)
+    pw.send_keys(myPw)
 
     browser.find_element(By.XPATH,'//*[@id="entry-login"]').click()
 

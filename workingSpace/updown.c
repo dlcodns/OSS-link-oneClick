00e3e2dd35875bcd110f4768d4d81a07270bbe04
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-11-30 00:34:01 +0900

    업다운 룰, 게임 진행 메인함수 추가하기

--- workingSpace/updown.c
+++ workingSpace/updown.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 
 int ran;
+int count = 0;
 
 int any()
 {
@@ -9,4 +10,41 @@ int any()
 	ran = rand() % 100 + 1;
 
 	return ran;
+}
+
+int main(void)
+{
+	int num;
+	ran = any();
+	//printf("%d", ran);
+	
+	while(1){
+		num = 0;
+
+		printf("숫자 하나를 적으세요. = ");
+		scanf_s("%d", &num);
+
+		if (ran > num) {
+			if (count != 5) {
+				printf("up!\n");
+			}
+
+		}
+		else if (ran == num) {
+			printf("딩동댕~ 정답!\n");
+				break;
+		}
+		else if (ran < num) {
+			if (count != 5) {
+				printf("down!\n");
+			}
+			
+		}
+		count += 1;
+		if (count == 6) {
+			printf("\n정답은 %d 였습니다~ 메롱 ㅋㅋㄹㅃㅃ", ran);
+				break;
+		}
+	}
+	return 0;
 }

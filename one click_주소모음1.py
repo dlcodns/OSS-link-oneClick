6d6a98a764743b7b533e2569e600ec58f2c5e0dc
songyeongeun <107869024+songyeongeun@users.noreply.github.com>
2022-11-29 15:03:58 +0900

    버튼색변경 + 버튼 추가

--- one click_주소모음1.py
+++ one click_주소모음1.py
@@ -63,28 +63,30 @@ def check_data():
         print("Check your Username/Password")
 
 #원클릭 넣을 이미지라벨
-label=tkinter.Label(window,text="ccc",width=200, height=105,relief="solid")
-label.pack()
+label1=tkinter.Label(window,text="ccc",width=40, height=10,relief="solid")
+label1.place(x=127,y=30)
 
 #휴게소 라벨(이미지 넣어야하고 게임시작 버튼 넣어야함)
-label=tkinter.Label(window, text="휴게소", width=44,height=15, fg="blue",relief="solid")
-label.place(x=113,y=215)
+label2=tkinter.Label(window, text="휴게소", width=44,height=15, fg="blue",relief="solid")
+label2.place(x=113,y=215)
 
 #폰트 설정
-font=tkinter.font.Font(family="맑은 고딕", size=10, weight="bold")
+font=tkinter.font.Font(family="", size=10, weight="bold")
 
 #주소모음 버튼
-b1=tkinter.Button(window, text="충북대학교\n홈페이지",bg="white", font=font,command=createLoginWindow)
-b2=tkinter.Button(window, text="충북대학교\n씨앗",bg="white", font=font,command=createLoginWindow)
-b3=tkinter.Button(window, text="충북대학교\n학생생활관",bg="white", font=font,command=createLoginWindow)
-b4=tkinter.Button(window, text="충북대학교\necampus",bg="white", font=font,command=createLoginWindow)
-b5=tkinter.Button(window, text="충북대학교\n개신누리",bg="white", font=font,command=createLoginWindow)
-b6=tkinter.Button(window, text="충북대학교\n취업지원본부",bg="white", font=font,command=createLoginWindow)
+b1=tkinter.Button(window, text="충북대학교\n홈페이지",bg="pink1", font=font,command=createLoginWindow)
+b2=tkinter.Button(window, text="충북대학교\necampus",bg="pink1", font=font,command=createLoginWindow)
+b3=tkinter.Button(window, text="충북대학교\n학생생활관",bg="pink1", font=font,command=createLoginWindow)
+b4=tkinter.Button(window, text="충북대학교\n개신누리",bg="pink1", font=font,command=createLoginWindow)
+b5=tkinter.Button(window, text="충북대학교\n씨앗",bg="pink1", font=font,command=createLoginWindow)
+b6=tkinter.Button(window, text="충북대학교\n취업지원본부",bg="pink1", font=font,command=createLoginWindow)
 
 b7=tkinter.Button(window, text="백준",bg="white", font=font)
 b8=tkinter.Button(window, text="코드업",bg="white", font=font)
 b9=tkinter.Button(window, text="replit",bg="white", font=font)
-b10=tkinter.Button(window, text="충북대학교\n전화번호",bg="white", font=font,command=createNumberWindow)
+b10=tkinter.Button(window, text="sw사업단\n공지사항",bg="white", font=font)
+b11=tkinter.Button(window, text="컴퓨터공학과\n공지사항",bg="white", font=font)
+b12=tkinter.Button(window, text="충북대학교\n전화번호",bg="white", font=font,command=createNumberWindow)
 
 #주소버튼 절대위치
 b1.place(x=20,y=30,width=80,height=50)
@@ -98,6 +100,8 @@ b7.place(x=440,y=30,width=80,height=50)
 b8.place(x=440,y=100,width=80,height=50)
 b9.place(x=440,y=170,width=80,height=50)
 b10.place(x=440,y=240,width=80,height=50)
+b11.place(x=440,y=310,width=80,height=50)
+b12.place(x=440,y=380,width=80,height=50)
 
 
 window.mainloop()

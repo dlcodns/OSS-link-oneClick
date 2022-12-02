ae9a67405a34eaa1a2c671a2276a36c94eb23978
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-12-02 16:10:51 +0900

    창 띄우는 코드

--- workingSpace/updown.py
+++ workingSpace/updown.py
@@ -1,1 +1,11 @@
+import tkinter
+import tkinter.font
+import tkinter.ttk
 
+window = tkinter.Tk()
+window.title("창 연습")
+window.geometry("500x400")
+window.resizable(False,False)
+window['bg']='olivedrab'
+
+window.mainloop()

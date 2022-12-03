03cac86512a6572ce8225fa24f4c53637b8e949a
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-12-04 00:04:08 +0900

    버튼1 "게임시작" 생성

--- workingSpace/updown.py
+++ workingSpace/updown.py
@@ -3,9 +3,14 @@ import tkinter.font
 import tkinter.ttk
 
 window = tkinter.Tk()
-window.title("창 연습")
+window.title("업다운 게임")
 window.geometry("500x400")
 window.resizable(False,False)
 window['bg']='olivedrab'
+
+font=tkinter.font.Font(family="맑은고딕", size=10, weight="bold")
+  
+b1=tkinter.Button(window, text="게임 시작",bg="olivedrab",relief="raised",borderwidth=1,font=font,command=updown)
+b1.place(x=200,y=300,width=100,height=50)
 
 window.mainloop()

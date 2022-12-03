81b15c58c9ae9874d49a2a7ee18c6539e363b131
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-12-04 00:04:56 +0900

    대략적인 구조 만들기, 게임 종료 버튼 만들기

--- workingSpace/updown.py
+++ workingSpace/updown.py
@@ -9,6 +9,22 @@ window.resizable(False,False)
 window['bg']='olivedrab'
 
 font=tkinter.font.Font(family="맑은고딕", size=10, weight="bold")
+
+def updown():
+  b2=tkinter.Button(window, text="게임 종료",bg="olivedrab",relief="raised",borderwidth=1,font=font,command=exit_game)
+  b2.place(x=200,y=300,width=100,height=50)
+  
+  tkinter.Label(window, text="숫자 하나를 적으세요.",bg="darkslategray",fg="white",borderwidth=2,relief="raised").place(x=190,y=130,width=124,height=30)
+    
+  number = tkinter.StringVar()
+  number_entered = tkinter.ttk.Entry(window, width=20, textvariable=number)
+  number_entered.place(x=180,y=180)
+  
+  action = tkinter.Button(window, text="확인")
+  action.place(x=330,y=178)
+  
+def exit_game():
+    window.destroy()
   
 b1=tkinter.Button(window, text="게임 시작",bg="olivedrab",relief="raised",borderwidth=1,font=font,command=updown)
 b1.place(x=200,y=300,width=100,height=50)

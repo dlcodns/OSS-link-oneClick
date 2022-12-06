f671116e550ba979510137440ba47f2d362d2d24
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-12-06 16:20:52 +0900

    반복되는 오류 수정
    whlie문을 없애고 확인 버튼을 클릭 할 때마다 if문을 반복 실행하는 것으로 바꾸었다,

--- workingSpace/updown.py
+++ workingSpace/updown.py
@@ -18,50 +18,55 @@ ran = random.randint(1,100)
 count = 0
 
 def updown():
-  global ran, count
-
-  b2=tkinter.Button(window, text="게임 종료",bg="olivedrab",relief="raised",borderwidth=1,font=font2,command=exit_game)
-  b2.place(x=200,y=300,width=100,height=50)
+    global ran, count
   
-  while(True):
-        tkinter.Label(window, text="숫자 하나를 적으세요.",bg="darkslategray",fg="white",borderwidth=2,relief="raised").place(x=190,y=130,width=124,height=30)
+    b2=tkinter.Button(window, text="게임 종료",bg="olivedrab",relief="raised",borderwidth=1,font=font2,command=exit_game)
+    b2.place(x=200,y=300,width=100,height=50)
     
-        number = tkinter.StringVar()
-        number_entered = tkinter.ttk.Entry(window, width=20, textvariable=number)
-        number_entered.place(x=180,y=180)
+ 
+    tkinter.Label(window, text="숫자 하나를 적으세요.",bg="darkslategray",fg="white",borderwidth=2,relief="raised", font=font1).place(x=190,y=130,width=124,height=30)
+
+    number = tkinter.StringVar()
+    number_entered = tkinter.ttk.Entry(window, width=20, textvariable=number)
+    number_entered.place(x=180,y=180)
+
+    action = tkinter.Button(window, text="확인", font=font1, command=lambda:[calc(number_entered)])
+    action.place(x=330,y=176)
     
-        action = tkinter.Button(window, text="확인", command=lambda:[calc(number_entered)])
-        action.place(x=330,y=176)
-        
-        if count == 6:
-            tkinter.Label(window, text="정답은 ran이었습니다! 메롱", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=160,y=220,width=200,height=30)
-            break
+
             
 def calc(enteredNum):
     global ran, count
     
     num = int(enteredNum.get())
-        
-    if ran > num:
-        if count != 5:
-            tkinter.Label(window, text="up!", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=190,y=220,width=124,height=50)
-    elif ran < num:
-        if count != 5:
-            tkinter.Label(window, text="down!", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=165,y=220,width=170,height=30)
-    elif ran == num:
-        win = random.randint(1,3)
-        if win == 1:
-            tkinter.Label(window, text="딩동댕~ 정답!", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=190,y=220,width=124,height=30)
-        if win == 2:
-            tkinter.Label(window, text="운이 좋으시네요. :)", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=190,y=220,width=124,height=30)
-        if win == 3:
-            tkinter.Label(window, text="올ㅋ",bg="olivedrab", font=font4, fg="white",borderwidth=2,relief="flat").place(x=190,y=220,width=124,height=30)
-    count += 1
-  
+     
+     
+     
+    if count < 6 :
+        if ran > num:
+            if count !=5:
+                tkinter.Label(window, text="up!", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=190,y=220,width=124,height=50)
+        elif ran < num:
+            if count !=5:
+                tkinter.Label(window, text="down!", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=165,y=220,width=170,height=30)
+        elif ran == num:
+            win = random.randint(1,3)
+            if win == 1:
+                tkinter.Label(window, text="딩동댕~ 정답!", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=190,y=220,width=124,height=30)
+            if win == 2:
+                tkinter.Label(window, text="운이 좋으시네요. :)", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=190,y=220,width=124,height=30)
+            if win == 3:
+                tkinter.Label(window, text="올ㅋ",bg="olivedrab", font=font4, fg="white",borderwidth=2,relief="flat").place(x=190,y=220,width=124,height=30)
+        count= count+1
+        if count == 6 :
+            tkinter.Label(window, text="정답은 ran이었습니다! 메롱", font=font4, bg="olivedrab",fg="white",borderwidth=2,relief="flat").place(x=160,y=220,width=200,height=30)
+
+
 def exit_game():
     window.destroy()
-  
+
+    
 b1=tkinter.Button(window, text="게임 시작",bg="olivedrab",relief="raised",borderwidth=1,font=font2,command=updown)
 b1.place(x=200,y=300,width=100,height=50)
-
+    
 window.mainloop()

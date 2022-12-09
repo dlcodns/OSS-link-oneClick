048050d1bef2f0aafaf0351fe28d2ea72b58450f
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-12-10 04:07:14 +0900

    진행 함수 입력

--- workingSpace/updown.py
+++ workingSpace/updown.py
@@ -7,10 +7,10 @@ import sys
 
 #윈도우 창 설정
 window = tkinter.Tk()
-window.title("업다운 게임")
+window.title("제비뽑기")
 window.geometry("500x400")
 window.resizable(False,False)
-window["bg"]="#d4e157"
+window["bg"]="light yellow"
 
 #폰트 크기 설정
 font1=tkinter.font.Font(family="맑은 고딕", size=10, weight="bold")
@@ -18,79 +18,62 @@ font2=tkinter.font.Font(family="맑은 고딕", size=15, weight="bold")
 font3=tkinter.font.Font(family="맑은 고딕", size=20, weight="bold")
 font4=tkinter.font.Font(family="맑은 고딕", size=25, weight="bold")
 
-#난수 설정
-ran = random.randint(1,100)
-count = 0
+#제비뽑기 진행 함수
+def jeabi():
+    jgrassLoopImglabel=tkinter.Label(window, image=jgrassLoopImg, relief="flat", bg="light yellow").place(x=-23,y=-41)
 
-#게임 진행 함수
-def updown():
-    #TcoverImgPath=resource_path("src/Tcover.png")
-    #TcoverImg=tkinter.PhotoImage(file = TcoverImgPath)
-    #TcoverImglabel=tkinter.Label(window, image=TcoverImg, relief="flat", bg="#d4e157").place(x=77.5,y=80)
-    #ScoverImgPath=resource_path("src/Scover.png")
-    #ScoverImg=tkinter.PhotoImage(file = ScoverImgPath)
-    #ScoverImglabel=tkinter.Label(window, image=ScoverImg, relief="flat", bg="#d4e157").place(x=164,y=230,width=175)
-    #CcoverImgPath=resource_path("src/Ccover.png")
-    #CcoverImg=tkinter.PhotoImage(file = CcoverImgPath)
-    #CcoverImglabel=tkinter.Label(window, image=CcoverImg, relief="flat", bg="#d4e157").place(x=164,y=230)
-    #loopImgPath=resource_path("src/loop.png")
-    #loopImg=tkinter.PhotoImage(file = loopImgPath)
-    #loopImglabel=tkinter.Label(window, image=loopImg, relief="flat", bg="#385723").place(x=30,y=40)
-    titleCover=tkinter.Label(window, bg="#d4e157",relief="flat").place(x=77,y=80,width=348,height=96)
-    startCover=tkinter.Label(window, bg="#d4e157",relief="flat").place(x=164,y=230,width=170,height=60)
-    calcCover=tkinter.Label(window, bg="#d4e157",relief="flat").place(x=164,y=230,width=170,height=60)
-    loopImg=tkinter.Label(window, bg="#385723",relief="flat").place(x=35,y=40,width=171,height=201)
+    #startCover=tkinter.Label(window, bg="light yellow",relief="flat").place(x=164,y=270,width=170,height=62)
+    #jtitleCover=tkinter.Label(window, bg="light yellow",relief="flat").place(x=150,y=160,width=200,height=62)
+    restartBtn=tkinter.Button(window, image=restartImg ,relief="flat", command=jeabi).place(x=125,y=285)
+    exitBtn=tkinter.Button(window, image=exitImg ,relief="flat", command=exit_game).place(x=277,y=285)
+    #재시작, 종료 버튼 만들기(완료)
     
-    global ran, count
+    #tkinter.Label(window, image=jnum,bg="darkslategray",fg="white",borderwidth=2,relief="raised").place(x=110,y=80,width=80,height=30)
+    jnumImglabel=tkinter.Label(window, image=jnumImg, relief="flat", bg="#FEE58A").place(x=90,y=80)
     
-    restartBtn=tkinter.Button(window, image=restartImg ,relief="flat", bg="#385723", command=updown).place(x=125,y=271)
-    exitBtn=tkinter.Button(window, image=exitImg ,relief="flat", bg="#385723", command=exit_game).place(x=277,y=271)
-    
-    #b1=tkinter.Button(window, text="재시작",bg="#385723",relief="flat",borderwidth=1,font=font2,command=updown)
-    #b1.place(x=120,y=271,width=100,height=50)
-    #b2=tkinter.Button(window, text="게임 종료",bg="#385723",relief="flat",borderwidth=1,font=font2,command=exit_game)
-    #b2.place(x=280,y=271,width=100,height=50)
-    
- 
-    tkinter.Label(window, text="숫자 하나를 적으세요.",bg="darkslategray",fg="white",borderwidth=2,relief="raised", font=font1).place(x=285,y=80,width=140,height=30)
-
     number = tkinter.StringVar()
-    number_entered = tkinter.ttk.Entry(window, width=20, textvariable=number)
-    number_entered.place(x=279,y=130)
-
-    action = tkinter.Button(window, text="확인", font=font1, command=lambda:[calc(number_entered)])
-    action.place(x=431,y=126)
+    number_entered = tkinter.ttk.Entry(window, font=font2, width=10, textvariable=number)
+    number_entered.place(x=260,y=87)
     
-#업다운 비교 함수          
-def calc(enteredNum):
-    global ran, count
+    action = tkinter.Button(window, text="확인", font=font1, command=lambda:[input(action, number_entered)])
+    action.place(x=395,y=91)
+    
+    #인원수num 입력하기(완료)
+    #2~8명 제한
+    #a배열 만들기
+    #역할 정하기 라벨(나열 후 전체 확인)
+    #
+    #shake(num)
+    #최종 제비들 나열
+    #클릭하면 역할 보이기
+    #
+
+#입력과 배열 설정
+def input(action, enteredNum):
+    action.config(state="disabled")
     
     num = int(enteredNum.get())
-     
-    if count < 6 :
-        if ran > num:
-            if count !=5:
-                tkinter.Label(window, text="up!", font=font4, bg="#d4e157",fg="white",borderwidth=2,relief="flat").place(x=273,y=170,width=170,height=50)
-        elif ran < num:
-            if count !=5:
-                tkinter.Label(window, text="down!", font=font4, bg="#d4e157",fg="white",borderwidth=2,relief="flat").place(x=273,y=170,width=170,height=50)
-        elif ran == num:
-            win = random.randint(1,3)
-            if win == 1:
-                tkinter.Label(window, text="딩동댕~ 정답!", font=font4, bg="#d4e157",fg="white",borderwidth=2,relief="flat").place(x=240,y=230,width=124,height=30)
-            if win == 2:
-                tkinter.Label(window, text="운이 좋으시네요. :)", font=font4, bg="#d4e157",fg="white",borderwidth=2,relief="flat").place(x=240,y=250,width=124,height=30)
-            if win == 3:
-                tkinter.Label(window, text="올ㅋ",bg="#d4e157", font=font4, fg="white",borderwidth=2,relief="flat").place(x=240,y=250,width=124,height=30)
-        count= count+1
-        msg = "정답은" + str(ran)
-        if count == 6 :
-            tkinter.Label(window, text=msg, font=font4, bg="#d4e157",fg="white",borderwidth=2,relief="flat").place(x=240,y=250,width=200,height=30)
+    if num>8 or num<2 :
+        print('2~8명만 가능합니다.')
+        jeabi()
+           
+    a = []
+    print('역할을 정해주세요.')
+    for i in range(0,num):
+        print('제비',i+1,' = ')
+        input(a[i])
+        
+    for j in range(0,num-1):
+        print('제비',i+1,' 역할: ',a[i])
+    #for i in range(0,):
+
+#난수 설정
+#def shake():
 
 #게임을 나가는 함수
 def exit_game():
     window.destroy()
-    
+
 # exe 제작을 위한 이미지 경로 설정 함수
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -103,22 +86,27 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 #기본 이미지
-playgroundImgPath=resource_path("src/playground.png")
-playgroundImg=tkinter.PhotoImage(file = playgroundImgPath)
-playgroundImglabel=tkinter.Label(window, image=playgroundImg, relief="flat", bg="#d4e157").place(x=-2,y=1)
-titleImgPath=resource_path("src/title.png")
-titleImg=tkinter.PhotoImage(file = titleImgPath)
-titleImglabel=tkinter.Label(window, image=titleImg, relief="flat", bg="#d4e157").place(x=77.5,y=80)
+#jtitleImgPath=resource_path("src/jtitle.png")
+#jtitleImg=tkinter.PhotoImage(file = jtitleImgPath)
+#jtitleImglabel=tkinter.Label(window, image=jtitleImg, relief="flat", bg="light yellow").place(x=105,y=80)
+jgrassImgPath=resource_path("src/jgrass.png")
+jgrassImg=tkinter.PhotoImage(file = jgrassImgPath)
+jgrassImglabel=tkinter.Label(window, image=jgrassImg, relief="flat", bg="light yellow").place(x=-23,y=-41)
+
+jgrassLoopImgPath=resource_path("src/jgrassLoop.png")
+jgrassLoopImg=tkinter.PhotoImage(file = jgrassLoopImgPath)
+jnumImgPath=resource_path("src/jnum.png")
+jnumImg=tkinter.PhotoImage(file = jnumImgPath)
 
 #버튼 이미지
-startImgPath=resource_path("src/start.png")
-startImg=tkinter.PhotoImage(file = startImgPath)
+jstartImgPath=resource_path("src/jstart.png")
+jstartImg=tkinter.PhotoImage(file = jstartImgPath)
 restartImgPath=resource_path("src/restart.png")
 restartImg=tkinter.PhotoImage(file = restartImgPath)
 exitImgPath=resource_path("src/exit.png")
 exitImg=tkinter.PhotoImage(file = exitImgPath)
 
 #시작 버튼 위치
-startBtn=tkinter.Button(window, image=startImg ,relief="flat", bg="#385723", command=updown).place(x=163.5,y=230)
+jstartBtn=tkinter.Button(window, image=jstartImg ,relief="flat",bg="light yellow", command=jeabi).place(x=174,y=270)
 
 window.mainloop()

685a1e472554943b6a0f88ed32eeeb9c942e63f8
chaeun lee <107669084+dlcodns@users.noreply.github.com>
2022-12-09 23:32:05 +0900

    배열 섞기 기능 추가

--- workingSpace/jeabi.c
+++ workingSpace/jeabi.c
@@ -6,12 +6,18 @@
 int ran;
 int count = 0;
 
-int any(int num)
+void any(int num, char *a)
 {
-	srand(time(NULL));
-	ran = rand() % num + 1;
-
-	return ran;
+	srand(time(NULL));  
+  int temp;
+  int ran;
+  for (int i=0;i<num-1;i++)
+  {
+    ran=rand()%(num-i)+i;
+    temp=a[i];
+    a[i]=a[ran];
+    a[ran]=temp;
+  }
 }
 
 int main(void) {
@@ -22,7 +28,6 @@ int main(void) {
     printf("2~8명만 가능합니다.\n\n");
     main();
   }
-  any(num);
   char a[num];
   printf("역할을 정해주세요.\n");
   for(int i=0;i<num;i++){
@@ -30,7 +35,9 @@ int main(void) {
     scanf("%s", &a[i]);
     printf("\n");
   }
-
+  
+  any(num,&a);
+  
   for(int i=0;i<num;i++){
     printf("제비%d 역할: %c",i+1, a[i]);
     printf("\n");
